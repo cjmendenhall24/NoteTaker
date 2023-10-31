f67f61736d37ae539e91ad955baeac7bcd35d6da
CJ <cjmendenhall24@hotmail.com>
2023-10-31 11:18:07 -0400

    update routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,21 +2,30 @@ const router = require('express').Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-router.get('/api/notes', async (req, res) => {
-    const jNotes = await JSON.parse(fs.readFileSync('./Develop/db/db.json', "utf8"));
-    res.json(jNotes);
+const readNotes = () => {
+    const data = fs.readFileSync('./Develop/db/db.json', 'utf8');
+    return JSON.parse(data);
+};
+
+const saveNotes = (notes) => {
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(notes));
+};
+
+router.get('/api/notes', (req, res) => {
+    const notes = readNotes();
+    res.json(notes);
 });
 
 router.post('/api/notes', (req, res) => {
-    const jNotes = JSON.parse(fs.readFileSync('./Develop/db/db.json', "utf8"));
-    const userNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4(),
-    };
-    jNotes.push(userNote);
-    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(jNotes));
-    res.json(jNotes);
+const notes = readNotes();
+const userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+};
+notes.push(userNote);
+saveNotes(notes);
+res.json(userNote);
 });
 
 module.exports = router;
